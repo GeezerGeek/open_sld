be214ba8bd3f2b6187983d360f3d1e7d7a78da29
GeezerGeek <vernm10@comcast.net>
2014-04-16 15:09:09 -0700

    Added files

---
+++ 245_decode.py
@@ -0,0 +1,288 @@
+#------------------------------------------------------------------------------
+#
+#   245_decode.py - 4/16/14
+#
+#   Decode captured FT245 data
+#
+#------------------------------------------------------------------------------
+
+import csv, pprint
+
+#------------------------------------------------------------------------------
+#
+# Globals
+
+signal_names = [
+    'step',
+    'tck',      # D0
+    'tms',      # D1
+    'nCE',      # D2
+    'nCS',      # D3
+    'tdi',      # D4
+    'LED',      # D5
+    'read',     # D6
+    'mode',     # D7
+    'SDR',
+    'UDR',
+    'WR'
+    ]
+
+# pprint.pprint(jtag_states)
+
+cpld_tck = 0
+cpld_tdi = 0
+cpld_tms = 0
+
+# Named CPLD mode constants
+CPLD_START = 0
+CPLD_BIT   = 1
+CPLD_BYTE  = 2
+
+RISING  = 1
+FALLING = 0
+
+#------------------------------------------------------------------------------
+#
+# A generator of captured data
+
+def cvs(file_name):
+
+    csvfile = open(file_name, 'rb')
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csv_reader:
+        
+#         print row
+        # Get this into a more usable format
+        dc = DataContainer(row)
+        
+#         print 'step:', dc.step
+#         if int(dc.step) == 21:
+#             break
+        
+        yield dc 
+        
+    csvfile.close()
+
+#------------------------------------------------------------------------------
+#
+# A container class for each row of captured data
+
+class DataContainer(object):
+    
+    def __init__(self, cvs_row):
+        
+        # Fields of cvs_row become attributes
+        d = dict((key, value.strip()) for (key, value) in zip(signal_names, cvs_row))
+#         pprint.pprint(d)
+        self.__dict__.update(d)
+        
+        self.cvs_row = cvs_row        
+               
+    #----
+    
+    @property
+    def byte_count(self):
+        # Getter property method for self.value
+        bits = [s.strip() for s in self.cvs_row[1:7]]
+        bits.reverse()
+        return int(''.join(bits), 2)
+               
+    #----
+    
+    @property
+    def bits(self):
+        # Getter property method for self.value
+        return [s.strip() for s in self.cvs_row[1:9]]
+               
+    #----
+    
+    @property
+    def now(self):
+        # Getter property method for self.now
+        return float(self.step) - 0.5
+
+#------------------------------------------------------------------------------
+#
+# CPLD Logic
+#
+# Process FT245 parallel output for bit & byte mode
+
+class CPLD(object):
+    
+    def __init__(self, byte_stream, mode):
+       
+        self.byte_stream = byte_stream
+        self.mode        = mode
+        
+    #----
+        
+    def run(self):
+    
+        try:
+        
+            new_byte   = self.byte_stream.next()
+            byte_count = 0
+            byte_read  = False
+            
+            while True:
+                
+                old_byte = new_byte
+                new_byte = self.byte_stream.next()
+                t = new_byte.now
+                
+                if self.mode == CPLD_START:
+                    # Wait for tck == 0
+                    if new_byte.tck == '0':
+                        self.mode = CPLD_BIT
+                        print 'EXIT START at ', new_byte.now
+                    continue
+
+                
+                if self.mode == CPLD_BIT:               
+                                        
+                    if new_byte.mode == '1':
+                        byte_count = new_byte.byte_count
+                        
+                        if new_byte.read == '1':
+                            byte_read = True
+                            print 'byte shift read mode %d' % byte_count
+                        else:
+                            byte_read = False
+                            print 'byte shift mode %d' % byte_count
+                            
+                        self.mode = CPLD_BYTE
+                        continue
+                
+                    if (new_byte.tck, old_byte.tck) == ('1', '0'):
+                        yield (RISING, new_byte.tms, new_byte.tdi, new_byte.read, new_byte.now)
+                        
+                    elif (new_byte.tck, old_byte.tck) == ('0', '1'):
+                        yield (FALLING, new_byte.tms, new_byte.tdi, new_byte.read, new_byte.now)
+                        
+                    
+                elif self.mode == CPLD_BYTE:
+                    
+                    # Byte mode
+                    if byte_count > 0:
+                                                    
+                        shift_bits = new_byte.bits
+                        for bit in shift_bits:
+                            yield (RISING,  '0', bit, byte_read, new_byte.now) 
+                            yield (FALLING, '0', bit, byte_read, new_byte.now)
+                        byte_count -= 1                           
+                    else:
+                        self.mode = CPLD_BIT
+
+                    continue
+                        
+      
+                t = new_byte.now
+                if t > 600.5:
+                    break
+                
+        except StopIteration:
+            print
+            print 'Done'
+
+#------------------------------------------------------------------------------
+#
+# SLD controller
+#
+# State
+
+class SLD_Controller(object):
+    
+    def __init__(self, node_count, start='reset'):
+ 
+        # A dict describing JTAG state transitions
+    
+        self.jtag_states = {
+            'capture_dr': ['shift_dr', 'exit1_dr'],
+             'capture_ir': ['shift_ir', 'exit1_ir'],
+             'exit1_dr': ['pause_dr', 'update_dr'],
+             'exit1_ir': ['pause_ir', 'update_ir'],
+             'exit2_dr': ['shift_dr', 'update_dr'],
+             'exit2_ir': ['shift_ir', 'update_ir'],
+             'idle': ['idle', 'select_dr'],
+             'pause_dr': ['pause_dr', 'exit2_dr'],
+             'pause_ir': ['pause_ir', 'exit2_ir'],
+             'reset': ['idle', 'reset'],
+             'select_dr': ['capture_dr', 'select_ir'],
+             'select_ir': ['capture_ir', 'reset'],
+             'shift_dr': ['shift_dr', 'exit1_dr'],
+             'shift_ir': ['shift_ir', 'exit1_ir'],
+             'update_dr': ['idle', 'select_dr'],
+             'update_ir': ['idle', 'select_dr']
+        }
+    
+        self.state = start
+#         self.state = 'shift_ir'
+        
+        self.ir_shift = ''
+        self.dr_shift = ''
+        
+        self.ir = ''
+        self.dr = ''
+        
+        self.node_count = node_count
+        
+    #----
+        
+    def TCK_rise(self, event):
+        
+        direction, tms, tdi, rd, t = event
+        self.new_state = self.jtag_states[self.state][int(tms)]
+        
+        if self.state == 'shift_ir':
+            self.ir_shift = tdi + self.ir_shift
+#             print '  ir_shift: %s' % self.ir_shift
+            
+        elif self.state == 'shift_dr':
+            self.dr_shift = tdi + self.dr_shift
+#             print '  dr_shift: %s' % self.dr_shift
+                                      
+    #----
+        
+    def TCK_fall(self, event):
+               
+        direction, tms, tdi, rd, t = event
+        
+        if self.new_state != self.state:
+            self.state = self.new_state
+            print '  TAP transition to: %s at %.1f' % (self.state, t)
+            
+            if self.state == 'capture_dr':
+                self.dr_shift = ''
+                
+            elif self.state == 'update_dr':
+                self.dr = self.dr_shift
+                print 'UPDATE %d dr bits to %s' % (len(self.dr), self.dr)
+                
+            elif self.state == 'capture_ir':
+                self.ir_shift = ''
+                
+            elif self.state == 'update_ir':
+                self.ir = self.ir_shift
+                print 'UPDATE %d ir bits to %s' % (len(self.ir), self.ir)
+
+                    
+#------------------------------------------------------------------------------
+#
+# Main
+
+# pld = CPLD(cvs('ft_245_data.csv'), CPLD_START)
+pld = CPLD(cvs('generated.csv'), CPLD_BIT)
+
+sld = SLD_Controller(1)
+    
+for event in pld.run():
+    direction, tms, tdi, rd, t = event
+    
+    if direction == RISING:
+        sld.TCK_rise(event)
+    else:
+        sld.TCK_fall(event)
+        
+#     if t > 65.0:
+#         break
+        
--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-open_sld
-========
+-------------------------------------------------------------------------------
+---                                                                         ---
+---                  Readme file for open_sld project                       ---
+---                                                                         ---
+-------------------------------------------------------------------------------
+  
+4/16/2014 - Vern Muhr (vernm10@comcast.net)  
 
-Direct Python interface to Altera SLD Mega-function
+The goal of this project is to create a python module that performs USB
+communication with the nodes of an SLD (System Level Debug) mega-function
+within an Altera FPGA.
+
+This project is at the proof of concept stage. The python module can write to
+to the InitialTest design created by Chris Zeh. Google:
+   "Talking to the DE0-Nano using the Virtual JTAG interface"
+   
+The project targets Windows. It should also be easy to target Linux.
+   
+Requirements:
+
+	FTD2XX.dll:
+	  http://www.ftdichip.com/Drivers/D2XX.htm)
+	  
+	Altera Quartus:
+	  http://www.altera.com/products/software/sfw-index.jsp)
+
+	pyftdi: 
+	  http://fluidmotion.dyndns.org/zenphoto/uploaded/ftdi/pyftdi.tar.gz
+	  
+	This project communicates with a DE0-Nano FPGA board via USB.
+	  http://www.terasic.com.tw/en/ Also available from Digikey, Mouser, Etc.		
+
+The files of this project are:
+
+sld_interface.py:
+
+    The high level interface to the FTD2XX routines, as well as the main
+    function that loads and drives the LEDs of the InitialTest FPGA design
+    Note: this file also contains a class that writes commands to a .CSV
+    file instead of the USB driver. This is useful for debugging, see 
+    245_decode.py.
+    
+245_decode.py:
+
+    A debug tool that emulates the SLD controller, and prints a log of TAP
+    controller states, and register values.
+    
+ft_245_data.csv:
+    A .cvs (comma seperated value) file of data captured on the FT245 parallel
+    interface pins. Open it with 245_decode.py to see a log of how the
+    captured data is processed by the SLD controller.
+
+ftdi.py:
+	The low-level python interface to FTD2XX.dll using ctypes. From the pyftdi
+	project. Named ftdi2.py in that project.
+	
+InitialTest.sof:
+    The programming file for the DE0-Nano FPGA. Loaded by sld_interface.py.
+	
+The details of how the FT245 parallel interface is used to control the JTAG
+pins of Altera FPGAs is documented here:
+  http://sourceforge.net/p/ixo-jtag/code/HEAD/tree/usb_jtag/
+
